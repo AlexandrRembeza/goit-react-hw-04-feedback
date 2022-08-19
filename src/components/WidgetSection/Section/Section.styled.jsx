5a6e5200b37b5cb8282e208a28ee8315839aba0e
AlexandrRembeza <99494287+AlexandrRembeza@users.noreply.github.com>
2022-08-19 22:35:54 +0300

    completed task

---
+++ src/components/WidgetSection/Section/Section.styled.jsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+export const WidgetSection = styled.section`
+  display: flex;
+  flex-direction: column;
+
+  padding: 20px 0;
+`;
+export const Title = styled.h1`
+  display: block;
+  font-size: 30px;
+  line-height: 1.2;
+  letter-spacing: 3px;
+
+  margin-top: 0;
+  margin-bottom: 30px;
+`;
